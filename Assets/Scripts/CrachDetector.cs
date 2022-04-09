0dbdc9e09c8ecaa11455f3875649fd83e53deabc
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-04-09 23:29:25 +0200

    bugs fix

--- Assets/Scripts/CrachDetector.cs
+++ Assets/Scripts/CrachDetector.cs
@@ -8,10 +8,11 @@ public class CrachDetector : MonoBehaviour
     [SerializeField] float ReloadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Ground")
+        if(other.tag == "Ground" && hasCrashed == false)
         {
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
