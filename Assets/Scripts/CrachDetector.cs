34c7f4e58083824f6bd48c0cc8ff7bdaad5e223d
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-04-09 23:03:21 +0200

    add audio effects
    add death sound and finish sound

--- Assets/Scripts/CrachDetector.cs
+++ Assets/Scripts/CrachDetector.cs
@@ -7,11 +7,13 @@ public class CrachDetector : MonoBehaviour
 {
     [SerializeField] float ReloadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", ReloadDelay);
         }
     }
